8f54ea3820902f9a86ac30e662838c95f4ceb770
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-03 20:44:36 +0100

    added vector images and classes

---
+++ src/components/Settings.jsx
@@ -0,0 +1,16 @@
+import Vector from "../assets/Vector.png"
+
+const Settings = () => {
+  return (
+    <>
+    <img src={Vector} alt="settings icon" className="first-type"/>
+   
+   <div className="top-vector">
+        <img src={Vector} alt="settings icon" className="second-type"/>
+        <img src={Vector} alt="settings icon" className="third-type"/>
+   </div>
+    </>
+  )
+}
+
+export default Settings
