aaa8a36f8308a9875f05795663f0ae6d79c9277d
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-03 12:18:11 +0100

    added text content and gummy image

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,7 +5,16 @@ import { Vector, Gummy } from "../assets"
 const Error = () => {
     return (
         <>
-        Error
+       <div>
+        <h1>OOPS!!!</h1>
+        <h2>404 error</h2>
+        <p>The page you are looking for can not be found.</p>
+        <button>Back to home</button>
+       </div>
+
+       <div className="">
+        <img src={Gummy} alt="" />
+       </div>
         </>
     )
 }
