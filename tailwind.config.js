b02815ae8233a5ce3e7962767ecaeb99dd34b19f
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-03 19:23:01 +0100

    fixing tailwind error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
+export const content = ["./src/**/*.{js,jsx}"];
+export const mode = "jit";
+export const theme = {
+  extend: {
+    colors: {
+      primary: "#E7DF2F",
+      secondary: "#fff",
+      
+    },
+    boxShadow: {
+      card: "0px 35px 120px -15px #211e35",
+    },
+    screens: {
+      xs: "450px",
+    },
+    
   },
-  plugins: [],
-}
-
+};
+export const plugins = []; 
