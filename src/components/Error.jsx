aedccbcb3b6938714ea4d13453a5b5379bd33fcf
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-03 19:23:16 +0100

    classes for styling

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import { Vector, Gummy } from "../assets"
+import { Gummy } from "../assets"
 
 
 const Error = () => {
     return (
         <>
-       <div>
-        <h1>OOPS!!!</h1>
-        <h2>404 error</h2>
-        <p>The page you are looking for can not be found.</p>
-        <button>Back to home</button>
+       <div className="main">
+        <h1 className="oops">OOPS!!!</h1>
+        <h2 className="error">404 error</h2>
+        <p className="para">The page you are looking for can not be found.</p>
+        <button className="button">Back to home</button>
        </div>
 
        <div className="">
