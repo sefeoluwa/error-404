78f8efa3a971adbcff51c6273179230bd800f260
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-03 19:42:12 +0100

    classes for css

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,14 +5,17 @@ const Error = () => {
     return (
         <>
        <div className="main">
-        <h1 className="oops">OOPS!!!</h1>
-        <h2 className="error">404 error</h2>
-        <p className="para">The page you are looking for can not be found.</p>
-        <button className="button">Back to home</button>
-       </div>
+            <div className="text-side">
+                <h1 className="oops">OOPS!!!</h1>
+                <h2 className="error">404 error</h2>
+                <p className="para">The page you are looking for can not be found.</p>
+                <button className="button">Back to home</button>
+           </div>
 
-       <div className="">
-        <img src={Gummy} alt="" />
+            <div className="image-side">
+                <img src={Gummy} alt="sad face cartoon" />
+            </div>
+
        </div>
         </>
     )
